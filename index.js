2255789840179bf9eeb719b29acb439dd5e594dc
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-10-01 08:26:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
-const BASE_URL = "https://striveschool-api.herokuapp.com/api/movies/";
-
-const myHeaders = new Headers();
-myHeaders.append(
-  "Authorization",
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFkMDkxOTJkNTI2MjAwMTViNmRjZjgiLCJpYXQiOjE2MzI0Nzg3OTcsImV4cCI6MTYzMzY4ODM5N30.evD1KyzoHTgpd_-9K0jNIWrZLufLLsVH3EQex6Gnpbw"
-);
-
-const getCategories = async () => {
+const getCategories = async (url) => {
   try {
-    const response = await fetch(BASE_URL, {
-        method: 'GET', 
-        headers: myHeaders});
-    if (response.ok) {
-        const categories = await response.json();
-        console.log("CATEGORIES ARR", categories);
-        const movies = await Promise.all(categories.map(async (category) => {
-            const resp = await fetch(BASE_URL + category, {
-                headers: myHeaders
-            })
-            return await resp.json()
-        }))
-        console.log("MOVIES", movies)
-    }
-  } catch (error) {
-    console.log("WE HAD A PROBLEM WITH THE FETCH: ", error);
+      const response = await fetch(url, {
+        headers: {
+          "Authorization":
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFkMDkxOTJkNTI2MjAwMTViNmRjZjgiLCJpYXQiOjE2MzI2ODA1MDksImV4cCI6MTYzMzg5MDEwOX0.MSSPVBa94MWIusyZ2_pN1QNSAUlWbdOOJfPPjC9mX_4"
+        }
+      })
+
+      if (!response.ok) { throw "Something went wrong with the fetch" }
+
+      const categories = await response.json()
+      console.log("CATEGORIES ARRAY", categories)
+      return categories
+
+  } catch (err) {
+      alert("WE HAVE AN ISSUE: " + err)
   }
-};
-
-displayMovies = (movies) => {
-    const movieSection = document.querySelectorAll(".container-fluid .pl-lg-5 .mt-3")
-    const movieRow = document.querySelectorAll(".row .m-n1")
-
-    const singleMovie = `<div class="col px-1">
-    <img
-      src="/assets/netflix_logo.png"
-      class="netflix-logo"
-      height="40px"
-      alt="Netflix Logo"
-    />
-    <img src=${movies.name} alt=${movies.name} />
-  </div>`
-
-    if (movies.length > 0) {
-        movies.forEach(movie => {
-            
-        });
-    }
 }
-{/* <div class="container-fluid pl-lg-5 mt-3">
-      <!--Container 1 row 1 trending -->
 
-      <div class="row m-n1">
+const getMovies = async (url, categories) => {
+  try {
+    const movies = await Promise.all(categories.map(async (category) => {
+      const r = await fetch(url + category, {
+        headers: {
+          "Authorization":
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFkMDkxOTJkNTI2MjAwMTViNmRjZjgiLCJpYXQiOjE2MzI2ODA1MDksImV4cCI6MTYzMzg5MDEwOX0.MSSPVBa94MWIusyZ2_pN1QNSAUlWbdOOJfPPjC9mX_4"
+        }
+      })
+      return await r.json()
+    })
+    )
+    console.log(movies)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/* const displayEvents = (array, containerNode) => {
+
+  console.log("TEST NODE", containerNode)
+
+  containerNode.innerHTML = ""
+
+  if (array.length > 0) {
+      array.forEach(event => {
+
+          const li = document.createElement("li")
+          li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center")
+          li.innerHTML = `<span>${event.name}</span><div><span class="badge badge-dark mr-3">${event.price}€</span><a href="./detail.html?eventId=${event._id}">VIEW DETAILS</a></div>`
+
+          containerNode.appendChild(li)
+      })
+  }
+} */
+
+{/* <div class="row m-n1">
         <h3>Trending Now</h3>
         <div class="d-flex flex-nowrap">
           <div class="col px-1">
@@ -105,9 +109,19 @@ displayMovies = (movies) => {
             <img src="/assets/media/media5.webp" alt="The China Hustle" />
           </div>
         </div>
-      </div>
-    </div> */}
+      </div> */}
 
-window.onload = () => {
-    getCategories()
+window.onload = async () => {
+  const url = "https://striveschool-api.herokuapp.com/api/movies/";
+  const movieSection = document.querySelector("div.row.m-n1")
+
+  try {
+      const categoriesArr = await getCategories(url)
+      
+      // displayEvents(events, eventsList)
+
+  } catch (err) {
+      console.log(err)
+  }
+
 }
